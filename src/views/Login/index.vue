12f6b194aeefbb3ca02919a2165bd0128a5156b3
unknown <you@example.com>
2019-12-18 22:59:49 +0800

    头条PC-登录模块-数据绑定及校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,22 +2,21 @@
   <div class="loginImg">
     <el-card class="loginBox">
       <div class="logintop"><img class="loginlogo"  src="../../assets/img/logo_index.png" alt=""></div>
-      <el-form ref='myform'>
-        <el-form-item>
-          <el-input placeholder='请输入手机号'></el-input>
+      <el-form :model='loginForm' :rules='loginRules' ref="myform">
+        <el-form-item prop='mobile'>
+          <el-input placeholder='请输入手机号' v-model="loginForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style='width:65%' placeholder='请输入验证码'></el-input>
+        <el-form-item prop='code'>
+          <el-input style='width:65%' placeholder='请输入验证码' v-model="loginForm.code"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item >
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop='check'>
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style='width:100%' type="primary">登录</el-button>
+          <el-button style='width:100%' type="primary" @click="submitLogin">登录</el-button>
         </el-form-item>
       </el-form>
-
     </el-card>
   </div>
 </template>
@@ -26,10 +25,40 @@
 export default {
   data () {
     return {
-      myform: {
-
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入正确的手机格式' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '请输入正确的验证码格式' }
+        ],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('你必须同意才能登陆'))
+          }
+        } }]
       }
     }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myform.validate(function (isOK) {
+        if (isOK) {
+          // 发送请求
+          console.log('校验成功')
+        }
+      })
+    }
+
   }
 
 }
