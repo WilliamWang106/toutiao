ee177e85ca8c8b4a6a799355e3972090c49d48ee
unknown <you@example.com>
2019-12-21 00:56:54 +0800

    头条PC-主页模块-左侧导航菜单

--- src/components/Home/layout-aside.vue
+++ src/components/Home/layout-aside.vue
@@ -3,29 +3,29 @@
       <div class="title">
           <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-    <el-menu background-color="#353b4e" text-color='#adafb5' active-text-color="#ffd04b">
+    <el-menu router background-color="#353b4e" text-color='#adafb5' active-text-color="#ffd04b">
       <!-- 一级导航栏 没有下拉选项 -->
-      <el-menu-item>首页</el-menu-item>
+      <el-menu-item index='/Home'>首页</el-menu-item>
       <!-- 一级导航栏 有下拉选项 -->
-      <el-submenu>
+      <el-submenu index='1'>
         <template slot="title">
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>文章列表</el-menu-item>
-        <el-menu-item>评论管理</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index='/Home/publish'>发布文章</el-menu-item>
+        <el-menu-item index='/Home/articles'>文章列表</el-menu-item>
+        <el-menu-item index='/Home/comment'>评论管理</el-menu-item>
+        <el-menu-item index='/Home/material'>素材管理</el-menu-item>
       </el-submenu>
-      <el-submenu>
+      <el-submenu index='2'>
         <template slot="title">
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
+        <el-menu-item index='/Home/picture'>图文数据</el-menu-item>
+        <el-menu-item index='/Home/fansinfo'>粉丝概况</el-menu-item>
+        <el-menu-item index='/Home/fanspicture'>粉丝画像</el-menu-item>
+        <el-menu-item index='/Home/fanslist'>粉丝列表</el-menu-item>
       </el-submenu>
-      <el-menu-item>账户信息</el-menu-item>
+      <el-menu-item index='/Home/userinfo'>账户信息</el-menu-item>
     </el-menu>
   </div>
 </template>
