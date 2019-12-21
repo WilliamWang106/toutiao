3ae07414a20eaff7525c662b40129c431c29dfb7
unknown <you@example.com>
2019-12-21 22:41:35 +0800

    头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './Home/layout-aside'
 import layoutHead from './Home/layout-head'
+import breadCrumb from './common/bread-crumb.vue'
 
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-head', layoutHead)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
