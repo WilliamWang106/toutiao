9c8e1e5f1f36edc0eaabb51888d4ef453d4d7c0d
unknown <you@example.com>
2019-12-22 10:16:32 +0800

    头条PC-评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -10,14 +10,15 @@
     <!-- 下面写数据 body el-table表格组件-->
     <el-table :data='list'>
       <el-table-column prop='title' label='标题' width='600'></el-table-column>
-      <el-table-column prop='comment_status' :formatter="commentBealoon" label='评论状态'></el-table-column>
+      <el-table-column :formatter="commentBealoon" label='评论状态'></el-table-column>
       <el-table-column prop='total_comment_count' label='总评论数'></el-table-column>
       <el-table-column prop='fans_comment_count' label='粉丝评论数'></el-table-column>
       <el-table-column label='操作'>
         <template slot-scope="obj">
           <el-button size="small" type="text">修改</el-button>
-          <el-button size="small" type="text">{{obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
+          <el-button @click='openOrClose(obj.row)' size="small" type="text">{{obj.row.comment_status ? '关闭' : '打开'}}评论</el-button>
         </template>
+        <!-- prop='comment_status' -->
 
       </el-table-column>
     </el-table>
@@ -43,6 +44,19 @@ export default {
     },
     commentBealoon (row, column, cellValue, index) {
       return cellValue ? '正常' : '关闭'
+    },
+    openOrClose (row) {
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`你是否确定要${mess}评论么？`, '提示').then(() => {
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id },
+          data: { allow_comment: !row.comment_status }
+        }).then(res => {
+          this.getComment()
+        })
+      })
     }
   },
   created () {
